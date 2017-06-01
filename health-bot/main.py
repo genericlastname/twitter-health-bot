a28fb1845b3d79e5dc30c0e9195192466627da1d
Kyle Brown <brownkylej@gmail.com>
2017-06-01 15:06:33 -0500

    Start work on tokenizer

--- health-bot/main.py
+++ health-bot/main.py
@@ -1,8 +1,20 @@
 import tweepy
 from tweepy import OAuthHandler
+import json
+from nltk.tokenize import word_tokenize
 
 
-def main():
+path = 'tweet_data.json'
+file = open(path, 'w+')
+
+
+def dump_data(tweet):
+    #file.write(json.dumps(tweet))
+    file.write(tweet)
+    file.write('\n')
+
+
+def collect_tweets():
     consumer_key = 'wka2iuZW3Aky7SFhwA8PhAGnl'
     consumer_secret = 'Swqq8AgjFRnKTEg3gmyBJDUZ8fnzqdfhfDirhn8owj0czog2vf'
     access_token = '869984091692822528-ykmxl5GO0FnISwSXjLMGmTC6FOvmgkU'
@@ -13,9 +25,22 @@ def main():
 
     api = tweepy.API(auth)
 
-    for status in tweepy.Cursor(api.home_timeline).items(10):
-        print(status.text)
+    print('Using file {}'.format(path))
+
+    for tweet in tweepy.Cursor(api.home_timeline).items(10):
+        dump_data(tweet.text)
+
+    file.close()
+
+
+def tokenize_tweets():
+    with open(path, 'r') as f:
+        line = f.readline()
+        print(word_tokenize(line))
+
+        f.close()
 
 
 if __name__ == '__main__':
-    main()
+    collect_tweets()
+    tokenize_tweets()
